288c79564f46f369a295304708e51f9611edf2b0
AlexFolland <lexlexlex@gmail.com>
2014-11-24 19:25:46 -0800

    initial commit

---
+++ Assets/fps.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class fps : MonoBehaviour {
+
+    Text txt;
+    int frameCount = 0;
+    float dt = 0.0f;
+    float framerate = 0.0f;
+    float updateRate = 60.0f; // number of updates per sec.
+
+	// Use this for initialization
+	void Start () {
+        txt = gameObject.GetComponent<Text>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        frameCount++;
+        dt += Time.deltaTime;
+        if (dt > 1.0f/updateRate)
+        {
+            framerate = frameCount / dt ;
+            frameCount = 0;
+            dt -= 1.0f/updateRate;
+        }
+        txt.text = Mathf.Floor(framerate).ToString() + " fps";
+	}
+}
---
+++ Assets/player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+    Transform link;
+    Transform playerWithCamera;
+    public float movementSpeed = 8f;
+
+	// Use this for initialization
+	void Start () {
+        link = transform.FindChild("Link");
+        playerWithCamera = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //movement
+        Vector3 playerMovementVector = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical")).normalized*movementSpeed*Time.deltaTime;
+		playerWithCamera.Translate(playerMovementVector);
+
+        //face the cursor
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        if(hit.collider.gameObject.tag == "Terrain"){
+            Vector3 lookTarget = hit.point;
+            lookTarget.y += link.transform.position.y; // character's vertical position; a little above ground
+            link.transform.LookAt(lookTarget);
+        }
+        else if(hit.collider.gameObject.tag == "Floor")
+        {
+            Vector3 lookTarget = hit.point;
+            lookTarget.y += link.transform.position.y; // character's vertical position; a little above ground
+            link.transform.LookAt(lookTarget);
+        }
+	}
+}
