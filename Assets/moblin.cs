d251ee10d0fdb2e1dd2d3cbfe23fd9b5e8770a7e
AlexFolland <lexlexlex@gmail.com>
2014-11-25 15:42:52 -0800

    add moblin prefab files

---
+++ Assets/moblin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moblin : MonoBehaviour {
+    Transform playerWithCamera;
+    public float movementSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+        playerWithCamera = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moblinMovementVector = (playerWithCamera.position - transform.position).normalized*movementSpeed*Time.deltaTime;
+        transform.Translate(moblinMovementVector * movementSpeed);
+	}
+}
