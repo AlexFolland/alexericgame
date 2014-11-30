b5c40abf319f75d1c6ab196a19facd8dd5be4350
AlexFolland <lexlexlex@gmail.com>
2014-11-30 07:48:34 -0800

    okay, he slashes fully on every swing.  now to fix the trail problems

--- Assets/sword.cs
+++ Assets/sword.cs
@@ -4,43 +4,61 @@ using System.Collections;
 public class sword : MonoBehaviour {
     private Animator animator;
     Component[] trailRenderers;
+    //Transform trailNode;
+    //Object trail;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+    {
         animator = GetComponent<Animator>();
         trailRenderers = GetComponentsInChildren<TrailRenderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(Input.GetButton("Fire1"))
+    }
+    
+    // Update is called once per frame
+	void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
         {
             //to do:
             //
-            //  -make animator inform me when it's done animating so I
-            //  can disable the trail at that point
-            //  -make the guy slash the full way every click
-            //  -make this function more efficient (only enable trail on animation start)
+            // -implement game
+            // -ship it
+            // -profit
+            // -live the dream
+            //
             ////////////////////////////////////////////////////////////////////
+
+            // if animating
+            if(animator.GetBool ("melee horizontal arc"))
+            {
+                //rewind to allow another animation to start
+                animator.Play("melee horizontal arc", -1, 0f);
+            }
 
             // play animation
             animator.SetBool("melee horizontal arc",true);
 
             // enable trail
-            foreach (Component trailNode in trailRenderers)
+            //trail = Instantiate (trailNode, new Vector3(0, 0, 0), Quaternion.identity);
+
+            foreach(Component trailNode in trailRenderers)
             {
                 trailNode.GetComponent<TrailRenderer>().enabled = true;
             }
-        }else{
-            //idle
-            animator.SetBool("melee horizontal arc",false);
-
-            // disable trail
-            foreach (Component trailNode in trailRenderers)
-            {
-                trailNode.GetComponent<TrailRenderer>().enabled = false;
-            }
         }
-        //animator.Update(Time.deltaTime);
 	}
+
+    public void AnimationEnd()
+    {
+        // disable trail
+        //Destroy (trail);
+
+        foreach (Component trailNode in trailRenderers)
+        {
+            trailNode.GetComponent<TrailRenderer>().enabled = false;
+        }
+        
+        //idle
+        animator.SetBool("melee horizontal arc",false);
+    }
 }
